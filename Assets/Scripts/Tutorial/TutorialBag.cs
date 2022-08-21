380c9c67a5dd0c3de72c41e97f78f60e21c54beb
micalbenitez <95487851+micalbenitez@users.noreply.github.com>
2022-08-21 17:34:44 -0300

    Added bag tutorial lerper

--- Assets/Scripts/Tutorial/TutorialBag.cs
+++ Assets/Scripts/Tutorial/TutorialBag.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Toolbox.Lerpers;
 
 namespace Tutorial
 {
@@ -8,16 +9,38 @@ namespace Tutorial
     {
         [Header("Bag positions")]
         public Vector3[] positions = null;
+        public float positionLerperSpeed = 0;
 
         /// Actual bag position
         private int bagPosition = 0;
+        /// Position lerper
+        private Vector3Lerper positionLerper = new Vector3Lerper();
+
+        private void Awake()
+        {
+            transform.localPosition = positions[0];
+        }
+
+        private void Update()
+        {
+            UpdatePositionLerper();
+        }
+
+        private void UpdatePositionLerper()
+        {
+            if (positionLerper.Active)
+            {
+                positionLerper.UpdateLerper();
+                transform.localPosition = positionLerper.GetValue();
+            }
+        }
 
         /// <summary>
         /// Call this to move the bag to the next position
         /// </summary>
         public void NextPosition()
         {
-            transform.localPosition = positions[bagPosition];
+            positionLerper.SetLerperValues(transform.localPosition, positions[bagPosition], positionLerperSpeed, Lerper<Vector3>.LERPER_TYPE.STEP_SMOOTH, true);
             bagPosition++;
             if (bagPosition >= positions.Length) bagPosition = 0;
         }
