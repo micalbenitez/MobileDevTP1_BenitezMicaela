2c5077e28727972fd5c927a409883eb8e07857e5
micalbenitez <95487851+micalbenitez@users.noreply.github.com>
2022-08-27 16:25:05 -0300

    Updated car camera script

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Entities.Player;
 
 namespace Managers
 {
@@ -10,10 +11,24 @@ namespace Managers
         public Camera cameraPlayer1 = null;
         public Camera cameraPlayer2 = null;
 
+        [Header("Car controller")]
+        public CarController carController1 = null;
+        public CarController carController2 = null;
+
+        private void Awake()
+        {
+            cameraPlayer1.gameObject.SetActive(false);
+            cameraPlayer2.gameObject.SetActive(false);
+            carController1.enabled = false;
+            carController2.enabled = false;
+        }
+
         public void StartGame()
         {
             cameraPlayer1.gameObject.SetActive(true);
             cameraPlayer2.gameObject.SetActive(true);
+            carController1.enabled = true;
+            carController2.enabled = true;
         }
     }
 }
