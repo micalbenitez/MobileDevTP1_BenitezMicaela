586697ce60afb55f0860b5119dec573791683801
micalbenitez <95487851+micalbenitez@users.noreply.github.com>
2022-09-07 21:16:36 -0300

    Improved main menu interaction

--- Assets/Scripts/GameConfiguration.cs
+++ Assets/Scripts/GameConfiguration.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Toolbox;
 
 public class GameConfiguration : MonoBehaviourSingleton<GameConfiguration>
 {
+    [Header("Players")]
+    [SerializeField] private Button[] playersButtons = null;
+
+    [Header("Difficult")]
+    [SerializeField] private Button[] difficultButtons = null;
+
+    [Header("Buttons data")]
+    [SerializeField] private Color unpressButton = Color.white;
+    [SerializeField] private Color pressButton = Color.white;
+
     [Header("Configuration")]
     [SerializeField] private int playersQuantity;
     [SerializeField] private int difficult;
 
-    public void SetPlayers(int value) => playersQuantity = value;
-    public int GetPlayers() => playersQuantity;
+    private void Start()
+    {
+        playersQuantity = 1;
+        difficult = 1;
+        ChangeButtonColor(playersButtons, playersQuantity - 1);
+        ChangeButtonColor(difficultButtons, difficult - 1);
+    }
 
-    public void SetDifficult(int value) => difficult = value;
+    private void ChangeButtonColor(Button[] buttons, int index)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i == index) buttons[i].image.color = pressButton;
+            else buttons[i].image.color = unpressButton;
+        }
+    }
+
+    public void SetPlayers(int value)
+    {
+        playersQuantity = value;
+        ChangeButtonColor(playersButtons, playersQuantity - 1);
+    }
+
+    public void SetDifficult(int value)
+    {
+        difficult = value;
+        ChangeButtonColor(difficultButtons, difficult - 1);
+    }
+
     public int Difficult() => difficult;
+    public int GetPlayers() => playersQuantity;
 }
