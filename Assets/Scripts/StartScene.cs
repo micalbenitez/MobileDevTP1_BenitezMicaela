786d9c6458df5ab9f162508629179257154d1f1e
micalbenitez <95487851+micalbenitez@users.noreply.github.com>
2022-09-27 00:03:15 -0300

    Edit start and loading scene

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Toolbox;
 
 public class StartScene : MonoBehaviour
 {
+    [Header("Loading bar")]
+    public Image loadingBar = null;
+
     const string mainMenuScene = "MainMenu";
+    private Timer timer = new Timer();
 
     private IEnumerator Start()
     {
+        timer.SetTimer(1, Timer.TIMER_MODE.INCREASE, true);
+        loadingBar.fillAmount = 0;
+
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(mainMenuScene);
         asyncLoad.allowSceneActivation = false;
         while (asyncLoad.progress < 0.9f)
@@ -18,5 +27,16 @@ public class StartScene : MonoBehaviour
         }
         yield return new WaitForSeconds(1f);
         asyncLoad.allowSceneActivation = true;
+    }
+
+    private void Update()
+    {        
+        if (timer.Active)
+        {
+            timer.UpdateTimer();
+            loadingBar.fillAmount = timer.CurrentTime;
+        }
+
+        if (timer.ReachedTimer()) loadingBar.fillAmount = 1;
     }
 }
